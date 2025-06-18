675342649153586cfb85c4f6406606b9a5817064
Mohit Rai <mohiit.rai.007@gmail.com>
2025-06-18 13:44:55 +0530

    Learned about math functions

---
+++ JavaScriptt/math.js
@@ -0,0 +1,25 @@
+// Math Functions In JavaScript
+
+let x=10
+let y=12
+let Z=12.56
+
+console.log(Math.round(Z))
+
+console.log(Math.floor(Z))
+
+console.log(Math.ceil(Z))
+
+console.log(Math.sqrt(16))
+
+console.log(Math.pow(9,2))
+
+console.log(Math.max(x,y))
+
+console.log(Math.min(x,y))
+
+console.log(Math.abs(-15))
+
+console.log(Math.sign(-17))
+
+console.log(Math.random(x,y))
