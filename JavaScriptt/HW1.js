4fda7ef2615bd8013c77da73676d990f36e8ddf2
Mohit Rai <mohiit.rai.007@gmail.com>
2025-06-18 12:28:04 +0530

    Done with the logic part of add and sub buttons

--- JavaScriptt/HW1.js
+++ JavaScriptt/HW1.js
@@ -1,3 +1,17 @@
+// ADDITION
+
 document.getElementById("add").onclick = function(){
-    
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
+    let calculation = num1+num2;
+    document.getElementById("result").textContent= "Addition of the two number is: " + calculation
+}
+
+// SUBTRACTION
+
+document.getElementById("sub").onclick = function(){
+    let num1 = document.getElementById("num1").value
+    let num2 = document.getElementById("num2").value
+    let calculation = num1-num2;
+    document.getElementById("result").textContent= "Addition of the two number is: " + calculation
 }
