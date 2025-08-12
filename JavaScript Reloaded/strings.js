73f65de1bf635f6df902d5fa8d2f2afffe4de87d
Mohit Rai <mohiit.rai.007@gmail.com>
2025-08-12 15:52:30 +0530

    strings Practice

---
+++ JavaScript Reloaded/strings.js
@@ -0,0 +1,11 @@
+let namec= "      Mohit Rai"
+
+console.log(namec.trim());
+console.log(namec.length);
+
+let sentence="Hello Everyone! My name is Mohit Rai."
+console.log(sentence.split(" "))
+
+let count_Words= "this is javascript react class"
+let count_words_array= count_Words.split(" ")
+console.log(count_words_array.length)
